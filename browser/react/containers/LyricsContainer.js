dd687c16299c1b6be40926f2aa229326e0b4faf6
TinaHeiligers <tina.heiligers@gmail.com>
2017-03-31 11:22:44 -0400

    wired up store with lyrics

--- browser/react/containers/LyricsContainer.js
+++ browser/react/containers/LyricsContainer.js
@@ -1,20 +1,67 @@
-import React, {Component} from 'react'
-import store from '../../redux/store.js'
+import React, {Component} from 'react';
+import store from '../../redux/store';
+import Lyrics from '../components/Lyrics';
+import {setLyrics} from '../../redux/action-creators/lyrics.js';
+import axios from 'axios';
 
-export default class LyricsContainer extends Component{
+export default class extends Component {
+
   constructor() {
-    super()
-    this.state = store.getState()
+
+    super();
+
+    this.state = Object.assign({
+      artistQuery: '',
+      songQuery: ''
+    }, store.getState());
+
+    this.handleArtistInput = this.handleArtistInput.bind(this);
+    this.handleSongInput = this.handleSongInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
   componentDidMount() {
-    this.unsubscribe = store.subscribe(() => this.setState(store.getState()));
+    this.unsubscribe = store.subscribe(() => {
+      this.setState(store.getState());
+    });
   }
 
   componentWillUnmount() {
     this.unsubscribe();
   }
 
-  render () {
-    return (<h1>Just a dumb container</h1>)
+  handleArtistInput(artist) {
+    this.setState({ artistQuery: artist });
   }
+
+  handleSongInput(song) {
+    this.setState({ songQuery: song });
+  }
+
+  handleSubmit(event) {
+
+    event.preventDefault();
+    if (this.state.artistQuery && this.state.songQuery) {
+
+      axios.get(`/api/lyrics/${this.state.artistQuery}/${this.state.songQuery}`)
+        .then(response => {
+          const setLyricsAction = setLyrics(response.data.lyric);
+          store.dispatch(setLyricsAction);
+        });
+
+    }
+
+  }
+
+  render() {
+    return <Lyrics
+      text={this.state.text}
+      setArtist={this.handleArtistInput}
+      setSong={this.handleSongInput}
+      artistQuery={this.state.artistQuery}
+      songQuery={this.state.songQuery}
+      handleSubmit={this.handleSubmit}
+    />
+  }
+
 }
